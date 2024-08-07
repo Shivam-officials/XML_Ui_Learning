ddf0a7112b39fe89e5900fd0419509202a19d7c6
Shivam <suryabhardwajofficial@gmail.com>
2024-08-07 16:04:47 +0530

    made fragment for input data

--- app/src/main/java/com/shivam/xmluilearning/MainActivity.kt
+++ app/src/main/java/com/shivam/xmluilearning/MainActivity.kt
@@ -7,29 +7,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.shivam.xmluilearning.databinding.ActivityMainBinding
+import com.shivam.xmluilearning.databinding.LayoutBenefitsBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        /**
+
         // Check if the fragment container is null to avoid overlapping fragments
         if (savedInstanceState == null) {
             // Create an instance of the fragment
-            val fragment = screen5()
+            val fragmentScreen5 = screen5()
+            val firebaseLearningFragment = FragmentFirebaseLearning()
 
             // Attach the fragment to the activity
             supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, fragment)
+                .replace(R.id.fragment_container,firebaseLearningFragment )
                 .commit()
         }
-        */
+
     }
 }
