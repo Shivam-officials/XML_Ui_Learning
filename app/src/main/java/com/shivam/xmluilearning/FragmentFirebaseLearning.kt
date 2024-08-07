6b49bdf6084c649abcad5ed89060d84409bf29da
Shivam <suryabhardwajofficial@gmail.com>
2024-08-07 16:35:51 +0530

    app connection to realtime database firebase

--- app/src/main/java/com/shivam/xmluilearning/FragmentFirebaseLearning.kt
+++ app/src/main/java/com/shivam/xmluilearning/FragmentFirebaseLearning.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.shivam.xmluilearning.databinding.FragmentFirebaseLearningBinding
+import com.shivam.xmluilearning.model.School
 
 
 class FragmentFirebaseLearning : Fragment() {
@@ -14,6 +18,7 @@ class FragmentFirebaseLearning : Fragment() {
     private var _binding: FragmentFirebaseLearningBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var firebaseRef: DatabaseReference
 
 
     override fun onCreateView(
@@ -28,6 +33,20 @@ class FragmentFirebaseLearning : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        firebaseRef = FirebaseDatabase.getInstance().getReference("test")
+
+        binding.idUploadButton.setOnClickListener{
+            val name = binding.idDrivingSchoolName.text.toString()
+            val address = binding.idSchoolAddress.text.toString()
+            val ratings = binding.idRatings.text.toString()
+            val ratingCount = binding.idRatingCount.text.toString()
+            val school = School(name,address,ratings,ratingCount)
+            firebaseRef.setValue(school)
+                .addOnCompleteListener {
+                    Toast.makeText(activity, "success", Toast.LENGTH_SHORT).show()
+                }
+        }
 
     }
 
---
+++ app/src/main/java/com/shivam/xmluilearning/model/School.kt
@@ -0,0 +1,8 @@
+package com.shivam.xmluilearning.model
+
+data class School(
+    val name: String,
+    val address: String,
+    val ratings: String,
+    val ratingCount: String
+)
